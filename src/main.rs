1f3eb7aff6b239f54df01cf85f4c06901b788d73
alianwaar73 <ali.github.anwaar73@gmail.com>
2025-09-04 16:09:18 +0500

    Separation of concerns in main.rs and lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,38 +18,10 @@ fn main() {
     println!("Path to file: {}", config.file_path);
     println!("Containing contents:\n");
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application errored: {e}");
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
-    let content = fs::read_to_string(config.file_path)?;
-
-    println!("{content}");
-
-    Ok(())
-}
-
-// Structure for Config type
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-// Creating the constructor Config for CLI argument parsing 
-impl Config {
-
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough input arguments provided.");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
